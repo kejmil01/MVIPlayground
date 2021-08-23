8898146093ba8daa8d63e625a62afea04fb0198b
kejmil01 <kejmil01@gmail.com>
2021-08-23 21:26:01 +0200

    Search scene finalized

--- app/src/main/java/net/fezzed/mviplayground/ui/home/HomeViewModel.kt
+++ app/src/main/java/net/fezzed/mviplayground/ui/home/HomeViewModel.kt
@@ -15,16 +15,17 @@ class HomeViewModel @Inject constructor(
     private val fetchHomeContentUseCase: FetchHomeContentUseCase
 ) : ViewModel() {
 
-    private var searchDiposable: Disposable? = null
-    private var query = ""
+    private var searchDisposable: Disposable? = null
 
-    val errorText = MutableLiveData<String>(null)
-    val errorVisible = errorText.map { !it.isNullOrEmpty() }
+    val query = MutableLiveData("")
     val loadingInProgress: MutableLiveData<Boolean> = MutableLiveData(false)
     val items: MutableLiveData<List<ItemModel>> = MutableLiveData(emptyList())
+    val noResultsTextVisible = MutableLiveData(false)
+    val errorText = MutableLiveData<String>(null)
+    val errorVisible = errorText.map { !it.isNullOrEmpty() }
 
     fun onSearchChanged(text: CharSequence) {
-        query = text.toString()
+        query.value = text.toString()
     }
 
     fun onSearchDoneAction(): Boolean {
@@ -33,10 +34,14 @@ class HomeViewModel @Inject constructor(
     }
 
     fun onSearchClick() {
-        searchDiposable?.dispose()
+        searchDisposable?.dispose()
+
         loadingInProgress.value = true
+        noResultsTextVisible.value = false
+        errorText.value = ""
+
         fetchHomeContentUseCase
-            .searchPeople(query)
+            .searchPeople(query.value ?: "")
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { content, error ->
                 content?.let {
@@ -46,15 +51,16 @@ class HomeViewModel @Inject constructor(
                         ItemModel(item.name, item.birth_year, item.height)
                     }
                     items.value = itemModeList
+                    noResultsTextVisible.value = itemModeList.isEmpty()
                 } ?: run {
                     errorText.value = error.message
                 }
                 loadingInProgress.value = false
-            }.also { searchDiposable = it }
+            }.also { searchDisposable = it }
     }
 
     override fun onCleared() {
-        searchDiposable?.dispose()
+        searchDisposable?.dispose()
         super.onCleared()
     }
 }
